15bbf2b9b0297e65ca6945af4d1373c72cc3fdba
Noëlle Rosenberg <noelle.rosenberg@simplificator.com>
2017-10-05 16:26:29 +0200

    try things out:
    - set a ship in playground
    - see if ship was hit or missed

--- src/app/field.component.ts
+++ src/app/field.component.ts
@@ -1,8 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 
+export const SHIP: Ship = {
+  startSquare: { x: 2, y: 5 },
+  endSquare: { x: 6, y: 5 }
+};
+
 export class Square {
   x: number;
   y: number;
+  hit?: boolean;
+  miss?: boolean;
+}
+
+export class Ship {
+  startSquare: Square;
+  endSquare: Square;
 }
 
 @Component({
@@ -12,6 +24,9 @@ export class Square {
 })
 export class FieldComponent implements OnInit {
   squares = this.generateFieldSquares();
+  ship = SHIP;
+  score = 0;
+  won = false;
 
   generateFieldSquares(): Square[] {
     const squares: Square[] = [];
@@ -22,6 +37,73 @@ export class FieldComponent implements OnInit {
     }
     return squares;
   }
+
+  getAllSquaresFrom(ship: Ship): Square[] {
+    let start = ship.startSquare.x;
+    let end = ship.endSquare.x;
+    let vertical = false;
+
+    if (ship.startSquare.x === ship.endSquare.x) {
+      vertical = true;
+      start = ship.startSquare.y;
+      end = ship.endSquare.y;
+    }
+
+    const squares: Square[] = [];
+    for (let _i = start; _i <= end; _i++) {
+      squares.push({ x: (vertical ? ship.startSquare.x : _i), y: (vertical ? _i : ship.startSquare.y) });
+    }
+
+    return squares;
+  }
+
+  shipHit(square: Square): boolean {
+    const squares = this.getAllSquaresFrom(this.ship);
+    let found = false;
+
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i].x === square.x && squares[i].y === square.y) {
+        found = true;
+        break;
+      }
+    }
+
+    return found;
+  }
+
+  checkIfWon(): void {
+    const squares = this.getAllSquaresFrom(this.ship);
+    let numberOfHits = 0;
+
+    for (let _i = 0; _i < squares.length; _i++) {
+      if (squares[_i].hit) {
+        numberOfHits++;
+      }
+    }
+
+    if (squares.length === numberOfHits) {
+      this.won = true;
+    }
+  }
+
+  onClick(square: Square): void {
+    console.log('geklickt', square.x, square.y);
+    console.log(this.getAllSquaresFrom(this.ship));
+
+    if (this.shipHit(square)) {
+      square.hit = true;
+    } else {
+      square.miss = true;
+    }
+
+    this.checkIfWon();
+    if (this.won) {
+      alert('You won!');
+    } else {
+      this.score++;
+    }
+  }
+
   ngOnInit(): void {
     this.generateFieldSquares();
   }
